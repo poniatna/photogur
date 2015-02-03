900b521917173b645d70fd29fdb830c36f0f8b56
poniatna <poniatna@users.noreply.github.com>
2015-02-03 18:51:03 -0500

    Adding feature to create new picture using HTML form

--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get "pictures" => 'pictures#index'
-  get 'pictures/:id' => 'pictures#show', as: 'picture'
+
+  post 'pictures' => 'pictures#create'
+  get 'pictures/new' => 'pictures#new'
   
+  get 'pictures/:id' => 'pictures#show', as: 'picture'
+
 
 
 
